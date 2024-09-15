ef69f819d548c518026bae65129eae7818d69606
Håkon <haakonu97@gmail.com>
2024-09-15 12:23:42 +0200

    chore: created hover effect for canvas rects

--- components/FloatingBitBackground.tsx
+++ components/FloatingBitBackground.tsx
@@ -1,14 +1,19 @@
 "use client";
+import { throttle } from "@/utils/throttle";
 import { useEffect, useRef } from "react";
 
 const AMOUNT_OF_BITS = 80;
 const getBitColor = () => "#073631";
 const getSpeed = () => rand(0.01, 0.2);
-const getSize = () => Math.random() * 2 + 2;
+const getSize = () => Math.round(Math.random() * 3 + 2);
 const SIZE_SPEED = 0.01;
 
 export function FloatingBitBackground() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const mousePos = useRef({
+    x: 0,
+    y: 0,
+  });
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -23,15 +28,40 @@ export function FloatingBitBackground() {
     }
 
     let lastFrame = 0;
-    const update = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      bits.forEach((bubble) => bubble.update(ctx));
-      lastFrame = requestAnimationFrame(update); // Create a loop
+    const animate = () => {
+      ctx.clearRect(
+        0,
+        0,
+        canvas.clientWidth,
+        canvas.clientHeight
+      );
+      bits.forEach((bubble) =>
+        bubble.update(ctx, mousePos.current)
+      );
+      lastFrame = requestAnimationFrame(animate);
     };
 
-    update(); // Start the animation loop
+    animate();
 
     return () => cancelAnimationFrame(lastFrame);
+  }, []);
+
+  useEffect(() => {
+    const listener = throttle((e: MouseEvent) => {
+      if (!canvasRef.current) return;
+      const rect = canvasRef.current.getBoundingClientRect();
+      const scaleX = canvasRef.current.width / rect.width;
+      const scaleY = canvasRef.current.height / rect.height;
+
+      // Mouse position
+      mousePos.current = {
+        x: (e.clientX - rect.left) * scaleX,
+        y: (e.clientY - rect.top) * scaleY,
+      };
+    }, 100);
+    document.addEventListener("mousemove", listener);
+    return () =>
+      document.removeEventListener("mousemove", listener);
   }, []);
 
   return <canvas ref={canvasRef} className="bg-canvas" />;
@@ -40,7 +70,7 @@ export function FloatingBitBackground() {
 class Bit {
   x: number;
   y: number;
-  radius: number;
+  size: number;
   fill: string;
   speed: number;
   hasFlipped: boolean;
@@ -57,34 +87,57 @@ class Bit {
     this.cHeight = height;
     this.x = Math.random() * width;
     this.y = height / 1.2 + (10 + i);
-    this.radius = getSize();
+    this.size = getSize();
     this.fill = fill;
     this.speed = getSpeed();
     this.hasFlipped = false;
     this.flipRadius = Math.random() * 5 + 5;
   }
 
-  update(ctx: CanvasRenderingContext2D) {
+  update(
+    ctx: CanvasRenderingContext2D,
+    mouse: {
+      x: number;
+      y: number;
+    }
+  ) {
     if (this.y < -this.cHeight || this.x < -this.cWidth) {
-      this.y = ctx.canvas.height + this.radius;
+      this.y = ctx.canvas.height + this.size;
       this.speed = getSpeed();
-      this.radius = getSize();
+      this.size = getSize();
       this.hasFlipped = false;
       this.x = Math.random() * this.cWidth;
     }
-    if (!this.hasFlipped && this.radius < this.flipRadius) {
-      this.radius += SIZE_SPEED;
+    if (!this.hasFlipped && this.size < this.flipRadius) {
+      this.size += SIZE_SPEED;
     }
-    if (this.radius >= this.flipRadius) {
+    if (this.size >= this.flipRadius) {
       this.hasFlipped = true;
     }
-    if (this.radius > 1 && this.hasFlipped) {
-      this.radius -= SIZE_SPEED;
+    if (this.size > 1 && this.hasFlipped) {
+      this.size -= SIZE_SPEED;
     }
 
     this.y -= this.speed;
-    ctx.fillRect(this.x, this.y, this.radius, this.radius);
-    ctx.fillStyle = this.fill;
+    const height = this.size;
+    const width = this.size;
+    if (
+      !inBounds(
+        {
+          x: this.x,
+          y: this.y,
+          width: 10,
+          height: 10,
+        },
+        mouse
+      )
+    ) {
+      ctx.fillStyle = this.fill;
+      ctx.fillRect(this.x, this.y, width, height);
+    } else {
+      ctx.fillStyle = "rgba(255, 255, 255, 0.2)";
+      ctx.fillRect(this.x, this.y, width, height);
+    }
     ctx.fill();
   }
 }
@@ -92,3 +145,22 @@ class Bit {
 const rand = (min: number, max: number) => {
   return Math.random() * (max - min) + min;
 };
+
+function inBounds(
+  a: {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+  },
+  b: {
+    x: number;
+    y: number;
+  }
+) {
+  const inBoundsHorizontally =
+    b.x >= a.x && b.x <= a.x + a.width;
+  const inBoundsVertically = b.y >= a.y && b.y <= a.y + a.height;
+
+  return inBoundsHorizontally && inBoundsVertically;
+}
